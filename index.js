01ed15c04480aac47f9f7b3c61569ff156e0aa60
Max LeLoup <52738651+MaxLeLoup@users.noreply.github.com>
2021-12-18 00:03:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { loadCommands, loadEvents } = require("./util/loader.js")
 const { TOKEN } = require('./config/config.js');
 
 const bot = new Client({ fetchAllMembers: true, partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'], intents: Object.keys(Intents.FLAGS) });
-["commands", "cooldowns"].forEach(x => client[x] = new Collection());
+["commands", "cooldowns"].forEach(x => bot[x] = new Collection());
 
 loadCommands(bot);
 loadEvents(bot);
