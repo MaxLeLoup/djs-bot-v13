9f4c5052718cada6bc55fee8feb628c0cf5aee0b
Max LeLoup <52738651+MaxLeLoup@users.noreply.github.com>
2021-12-18 00:01:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Client, Collection } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { loadCommands, loadEvents } = require("./util/loader.js")
 const { TOKEN } = require('./config/config.js');
 
-const bot = new Client();
+const bot = new Client({ fetchAllMembers: true, partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'], intents: Object.keys(Intents.FLAGS) });
 ["commands", "cooldowns"].forEach(x => client[x] = new Collection());
 
 loadCommands(bot);
